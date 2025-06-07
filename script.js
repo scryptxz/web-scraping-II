1966c5802265e78da7ef1f4fff0003fdcdbf1772
Renan <renanalmeida201612@gmail.com>
2025-06-07 17:08:42 -0300

    commit

---
+++ script.js
@@ -0,0 +1,87 @@
+function extractSongs(doc) {
+  const songs = doc.querySelectorAll(
+    "#recent-tracks-section > table > tbody > .chartlist-row"
+  );
+
+  const songsList = document.body.querySelector(".songs-list");
+
+  songs.forEach((el) => {
+    // Pegando os elementos do last.fm
+    const songTitle = el.querySelector(".chartlist-name > a").innerText;
+    const songArtist = el.querySelector(".chartlist-artist > a").innerText;
+    const songImage = el.querySelector(".chartlist-image > a > img")
+      ? el.querySelector(".chartlist-image > a > img").src
+      : el.querySelector(".chartlist-image > span > img").src;
+    const timestamp = el.querySelector(".chartlist-timestamp > span > a")
+      ? el.querySelector(".chartlist-timestamp > span > a").innerText.trim()
+      : el.querySelector(".chartlist-timestamp > span").innerText.trim();
+    // Criando elementos para a página
+    const li = document.createElement("li");
+    const songInfo = document.createElement("div");
+
+    const titleArtist = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
+
+    const img = document.createElement("img");
+    const span = document.createElement("span");
+
+    songInfo.className = "song-info";
+    titleArtist.className = "title-artist";
+
+    // Colocando as infos das músicas nos elementos
+
+    h2.innerText = songTitle;
+    p.innerText = songArtist;
+    img.src = songImage;
+    console.log(timestamp);
+    span.innerText =
+      timestamp === "Em scrobble no momento"
+        ? "Escutando agora..."
+        : timestamp
+
+    // Append em cada elemento
+    songsList.appendChild(li);
+    li.appendChild(songInfo);
+    songInfo.appendChild(img);
+    songInfo.appendChild(titleArtist);
+    titleArtist.appendChild(h2);
+    titleArtist.appendChild(p);
+    li.appendChild(span);
+
+    if (timestamp === "Em scrobble no momento") {
+      li.style.backgroundColor = "#251a1a"
+      span.style.color = "#e66060"
+      span.style.fontWeight = "600"
+    }
+  });
+
+  const loadingP = document.querySelector("#loading");
+  document.body.removeChild(loadingP);
+}
+
+async function fetchWebsite() {
+  const songsUL = document.querySelector(".songs-list");
+  const loadingP = document.querySelector("#loading");
+  loadingP.innerHTML = "Carregando...";
+  const dom = new DOMParser();
+  try {
+    const lastFmFetch = await fetch(
+      "https://corsproxy.io/?url=https://www.last.fm/pt/user/scryptrg"
+    );
+    const lastFmText = await lastFmFetch.text();
+    const lastFmDOM = dom.parseFromString(lastFmText, "text/html");
+    extractSongs(lastFmDOM);
+  } catch (error) {
+    songsUL.innerHTML = "Ocorreu algum erro.";
+    console.error(error);
+  }
+}
+
+function main() {
+  const btnMostrar = document.querySelector("#mostrar-btn");
+
+  btnMostrar.addEventListener("click", fetchWebsite);
+}
+
+window.onload = main;
